c1557d7d096508d4bd96b01598f412c29a4a0b5e
sthepaut <solene.thepaut@safrangroup.com>
2025-11-05 10:13:16 +0100

    Update base_system.py
    Add degradation of combustion chamber

--- performancemodel_ENSAI/src/performancemodel/base_system.py
+++ performancemodel_ENSAI/src/performancemodel/base_system.py
@@ -10,7 +10,7 @@ def h_SCSF(list_degrad=None, S=None, N=0.9, W3R=3, ratio=1100, as_dict=False):
     """Compute measurements thanks to the SCSF simulator
 
     Args:
-        list_degrad (list of list of float): list of degradation, first coordinate is compressor degradation, second coordinate is turbine degradatoin
+        list_degrad (list of list of float): list of degradation of size 2 or 3, first coordinate is compressor degradation, second coordinate is turbine degradation, third coordinate is combustion chamber degradation
         S (list of string, optional): sensors from which we want measurements Defaults to None.
         N (float, optional): engine speed in %. Defaults to 0.9.
         W3R (int, optional): Expected corrected airflow in compressor exit. Defaults to 3.
@@ -28,8 +28,13 @@ def h_SCSF(list_degrad=None, S=None, N=0.9, W3R=3, ratio=1100, as_dict=False):
         S = ["P3", "T4", "W5R"]
     
     out = []
-    for degrad in list_degrad:
-        turboreactor = SimpleCorpsSimpleFlux_degrad_n_ratio_beta(N=N, degrad_comp=degrad[0], degrad_turb=degrad[1], W3R_design=W3R, ratio_design=ratio)
+    for degrad in list_degrad
+        if len(list_degrad) == 2:
+            turboreactor = SimpleCorpsSimpleFlux_degrad_n_ratio_beta(N=N, degrad_comp=degrad[0], degrad_turb=degrad[1], W3R_design=W3R, ratio_design=ratio)
+        elif len(list_degrad) == 3:
+            turboreactor = SimpleCorpsSimpleFlux_degrad_n_ratio_beta(N=N, degrad_comp=degrad[0], degrad_turb=degrad[1], degrad_comb=degrad[2], W3R_design=W3R, ratio_design=ratio)
+        else:
+            print("Error, length of list_degrad should be 2 or 3")
         mes = turboreactor.run(S)
         
         out.append(mes)
@@ -37,4 +42,5 @@ def h_SCSF(list_degrad=None, S=None, N=0.9, W3R=3, ratio=1100, as_dict=False):
     if not as_dict:
         out = [np.array(list(x.values())) for x in out]
     
+
     return np.array(out)
